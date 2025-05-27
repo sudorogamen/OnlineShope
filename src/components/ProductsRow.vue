63dd27e6bee8d239e329d63c87ac609aa9f270ca
sudorogamen <sudorogamen@bk.ru>
2025-05-27 19:25:52 +0300

    первый коммит

---
+++ src/components/ProductsRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    productsArray: {
+      type: Array,
+    },
+  },
+  methods: {},
+};
+</script>
+<style scoped></style>
